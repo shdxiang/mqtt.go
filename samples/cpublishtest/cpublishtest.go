6752ab8fc69e280984777f93cc4aff95f026abc2
shdxiang <shdxiang@qq.com>
2016-07-19 18:46:51 +0800

    Save reg info to file

--- samples/cpublishtest/cpublishtest.go
+++ samples/cpublishtest/cpublishtest.go
@@ -1,30 +1,33 @@
 package main
 
 import (
+	"os"
+	"bufio"
 	"log"
 	"flag"
 	"time"
 	"strconv"
 	"sync"
+	"strings"
 	MQTT "github.com/shdxiang/mqtt.go"
 )
 
 var msgSent int = 0
 var msgRecv int = 0
-var wgSub sync.WaitGroup
+var wg sync.WaitGroup
 
 func defaultPublishHandler(client *MQTT.MqttClient, msg MQTT.Message) {
-	log.Printf("======TOPIC: %s\n", msg.Topic())
-	log.Printf("======MSG: %s\n", msg.Payload())
+	log.Printf("TOPIC: %s\n", msg.Topic())
+	log.Printf("MSG: %s\n", msg.Payload())
 }
 
 func onMessageReceived(client *MQTT.MqttClient, message MQTT.Message) {
-	log.Printf("======Received message on topic: %s\n", message.Topic())
-	log.Printf("======Message: %s\n", message.Payload())
+	log.Printf("Received message on topic: %s\n", message.Topic())
+	log.Printf("Message: %s\n", message.Payload())
 	msgRecv++
 }
 
-func doWork(index int, appkey *string, topic *string, qos int, message *string, pubCnt int, interval int) {
+func doReg(index int, regFile *os.File, appkey *string, topic *string, qos int, broker *string) {
 	deviceId := strconv.Itoa(time.Now().Second()) + strconv.Itoa(index)
 
 	yunbaClient := &MQTT.YunbaClient{*appkey, deviceId}
@@ -37,10 +40,8 @@ func doWork(index int, appkey *string, topic *string, qos int, message *string, 
 		log.Fatal("has error:", regInfo.ErrCode)
 	}
 
-	broker := "tcp://123.56.125.40:1883"
-
 	connOpts := MQTT.NewClientOptions()
-	connOpts.AddBroker(broker)
+	connOpts.AddBroker(*broker)
 	connOpts.SetClientId(regInfo.Client)
 	connOpts.SetCleanSession(true)
 	connOpts.SetProtocolVersion(0x13)
@@ -55,7 +56,47 @@ func doWork(index int, appkey *string, topic *string, qos int, message *string, 
 	if err != nil {
 		panic(err)
 	} else {
-		log.Printf("Connected to %s\n", broker)
+		log.Printf("Connected to %s\n", *broker)
+	}
+
+	filter, err := MQTT.NewTopicFilter(*topic, byte(qos))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client.StartSubscription(onMessageReceived, filter)
+
+	line := strings.Join([]string{regInfo.Client, regInfo.UserName, regInfo.Password}, "|")
+
+	log.Printf("line: %s\n", line)
+	writer := bufio.NewWriter(regFile)
+    _, err = writer.WriteString(line + "\n")
+    if err != nil {
+    	log.Fatal(err)
+    }
+
+    writer.Flush()
+	wg.Done()
+}
+
+func doWork(index int, clientid *string, user *string, pass *string, broker *string, topic *string, qos int, message *string, pubCnt int, interval int) {
+	connOpts := MQTT.NewClientOptions()
+	connOpts.AddBroker(*broker)
+	connOpts.SetClientId(*clientid)
+	connOpts.SetCleanSession(true)
+	connOpts.SetProtocolVersion(0x13)
+
+	connOpts.SetUsername(*user)
+	connOpts.SetPassword(*pass)
+
+	connOpts.SetDefaultPublishHandler(defaultPublishHandler)
+
+	client := MQTT.NewClient(connOpts)
+	_, err := client.Start()
+	if err != nil {
+		panic(err)
+	} else {
+		log.Printf("Connected to %s\n", *broker)
 	}
 
 	// <- client.SetAlias(deviceId)
@@ -67,53 +108,76 @@ func doWork(index int, appkey *string, topic *string, qos int, message *string, 
 
 	client.StartSubscription(onMessageReceived, filter)
 	// client.Presence(onMessageReceived, *topic)
-	wgSub.Done()
-	wgSub.Wait()
+	wg.Done()
+	wg.Wait()
 
 	time.Sleep(5 * time.Second)
 
 	for i := 0; i < pubCnt; i++ {
 		<- client.Publish(MQTT.QoS(qos), *topic, []byte(*message))
-		log.Printf("======Published\n")
+		log.Printf("Published\n")
 		time.Sleep(time.Duration(interval) * time.Millisecond)
 	}
 }
 
 func main() {
-	appkey := flag.String("appkey", "", "YunBa appkey")
-	topic := flag.String("topic", "", "Topic to publish the messages on")
+	log.SetPrefix("++++++")
+	appkey := flag.String("appkey", "563c4afef085fc471efdf803", "YunBa appkey")
+	topic := flag.String("topic", "topic_test", "Topic to publish the messages on")
 	message := flag.String("message", "hello", "Message to be published")
 	qos := flag.Int("qos", 0, "The QoS to send the messages at")
+	broker := flag.String("broker", "tcp://123.56.125.40:1883", "Broker address, default: tcp://123.56.125.40:1883")
 
-	routineNum  := flag.Int("routine", 1, "Number of goroutine")
-	ecahPubCnt := flag.Int("each", 1, "Each routine publish count")
+	client := flag.Int("client", 1, "Number of clients")
+	ecahPubCnt := flag.Int("each", 1, "Each client publish count")
 	interval := flag.Int("interval", 1000, "Publish interval for a routine, millisecond")
+
+	reg := flag.Bool("reg", false, "If register and save the returned infomation")
+	file := flag.String("file", "./reg.info", "Register infomation file")
 	//retained := flag.Bool("retained", false, "Are the messages sent with the retained flag")
 	flag.Parse()
 
-	if *appkey == "" {
-		log.Fatal("please set appkey")
-	}
-
-	if *topic == "" {
-		log.Fatal("please set topic")
-	}
-
-	for i := 0; i < *routineNum; i++ {
-		wgSub.Add(1)
-		go doWork(i, appkey, topic, *qos, message, *ecahPubCnt, *interval)
-		log.Printf("======Add: %d\n", i)
-		time.Sleep(10 * time.Millisecond)
-	}
-
-	msgNeedRecv := (*routineNum * *ecahPubCnt * *routineNum)
-
-	for {
-		time.Sleep(2 * time.Second)
-		if msgRecv == msgNeedRecv {
-			break
+	if *reg == true {
+		regFile, err := os.Create(*file)
+		if err != nil {
+			log.Fatal(err)
 		}
-		log.Printf("======msgRecv: %d\n", msgRecv)
+		defer regFile.Close()
+		for i := 0; i < *client; i++ {
+			wg.Add(1)
+			go doReg(i, regFile, appkey, topic, *qos, broker)
+			time.Sleep(10 * time.Millisecond)
+		}
+		wg.Wait()
+		// regFile.Sync()
+	} else {
+
+		regFile, err := os.Open(*file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer regFile.Close()
+		fileScanner := bufio.NewScanner(regFile)
+
+		cnt := 0
+		for fileScanner.Scan() {
+			
+			regInfo := strings.Split(fileScanner.Text(), "|")
+			wg.Add(1)
+			go doWork(cnt, &regInfo[0], &regInfo[1], &regInfo[2], broker, topic, *qos, message, *ecahPubCnt, *interval)
+			time.Sleep(10 * time.Millisecond)
+			cnt++
+		}
+
+		msgNeedRecv := (cnt * *ecahPubCnt * cnt)
+
+		for {
+			time.Sleep(2 * time.Second)
+			if msgRecv == msgNeedRecv {
+				break
+			}
+			log.Printf("msgRecv: %d\n", msgRecv)
+		}
+		log.Printf("msgRecv: %d\n", msgRecv)
 	}
-	log.Printf("======msgRecv: %d\n", msgRecv)
 }
