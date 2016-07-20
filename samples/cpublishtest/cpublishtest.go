725450fcf2528c101feecdc913d83df0e8110711
shdxiang <shdxiang@qq.com>
2016-07-20 21:48:28 +0800

    Add stat info

--- samples/cpublishtest/cpublishtest.go
+++ samples/cpublishtest/cpublishtest.go
@@ -6,9 +6,11 @@ import (
 	"log"
 	"flag"
 	"time"
+	"math"
 	"strconv"
 	"sync"
 	"strings"
+	"encoding/binary"
 	MQTT "github.com/shdxiang/mqtt.go"
 )
 
@@ -19,19 +21,32 @@ var wgSub sync.WaitGroup
 var wgRecv sync.WaitGroup
 var wgWork sync.WaitGroup
 
+var pubTimes [][]int64 // ns
+var usedTimes [][]int64 // ms
+
 func defaultPublishHandler(client *MQTT.MqttClient, msg MQTT.Message) {
 	log.Printf("TOPIC: %s\n", msg.Topic())
 	log.Printf("MSG: %s\n", msg.Payload())
 }
 
 func onMessageReceived(client *MQTT.MqttClient, message MQTT.Message) {
-	log.Printf("Received message on topic: %s\n", message.Topic())
+	ns := time.Now().UnixNano()
 	data := message.Payload()
-	l := len(data)
-	if l > 8 {
-		l = 8;
-	}
-	log.Printf("Message: %s\n", data[:l])
+
+	i := binary.LittleEndian.Uint32(data)
+	j := binary.LittleEndian.Uint32(data[4:])
+
+	usedTimes[i][j] += (ns - pubTimes[i][j]) / 1000000;
+
+	// log.Printf("recv msg len: %d", len(data))
+
+	// log.Printf("Received message on topic: %s\n", message.Topic())
+	// data := message.Payload()
+	// l := len(data)
+	// if l > 8 {
+	// 	l = 8;
+	// }
+	// log.Printf("Message: %s\n", data[:l])
 	msgRecv++
 }
 
@@ -61,7 +76,7 @@ func doReg(index int, regFile *os.File, appkey *string, topic *string, qos int, 
 	wgReg.Done()
 }
 
-func doWork(index int, clientid *string, user *string, pass *string, broker *string, topic *string, qos int, message *string, pubCnt int, interval int) {
+func doWork(index int, clientid *string, user *string, pass *string, broker *string, topic *string, qos int, msgLen int, pubEach int, interval int, doPub bool) {
 	connOpts := MQTT.NewClientOptions()
 	connOpts.AddBroker(*broker)
 	connOpts.SetClientId(*clientid)
@@ -89,15 +104,22 @@ func doWork(index int, clientid *string, user *string, pass *string, broker *str
 	// sub
 	client.StartSubscription(onMessageReceived, filter)
 
+	msg := make([]byte, msgLen)
+	binary.LittleEndian.PutUint32(msg, uint32(index))
+
 	wgSub.Done()
 	wgSub.Wait()
 
 	time.Sleep(2 * time.Second)
-
-	for i := 0; i < pubCnt; i++ {
-		<- client.Publish(MQTT.QoS(qos), *topic, []byte(*message))
-		log.Printf("Published\n")
-		time.Sleep(time.Duration(interval) * time.Millisecond)
+	
+	if doPub {
+		for i := 0; i < pubEach; i++ {
+			binary.LittleEndian.PutUint32(msg[4:], uint32(i))
+			pubTimes[index][i] = time.Now().UnixNano()
+			<- client.Publish(MQTT.QoS(qos), *topic, msg)
+			// log.Printf("Published\n")
+			time.Sleep(time.Duration(interval) * time.Millisecond)
+		}
 	}
 
 	wgRecv.Wait()
@@ -110,15 +132,16 @@ func doWork(index int, clientid *string, user *string, pass *string, broker *str
 func main() {
 	appkey := flag.String("appkey", "563c4afef085fc471efdf803", "YunBa appkey")
 	topic := flag.String("topic", "topic_test", "Topic to publish the messages on")
-	message := flag.String("message", "hello", "Message to be published")
+	msgLen := flag.Int("msglen", 8, "Length of message to be published, at least 8 bytes for statistics infomation")
 	qos := flag.Int("qos", 0, "The QoS to send the messages at")
 	broker := flag.String("broker", "tcp://123.56.125.40:1883", "Broker address, default: tcp://123.56.125.40:1883")
 
 	client := flag.Int("client", 1, "Number of clients")
-	pubCnt := flag.Int("pubcnt", 1, "Each client publish count")
-	interval := flag.Int("interval", 1000, "Publish interval for a client, millisecond")
+	pubClient := flag.Int("pubclient", 1, "Number of client who do publish")
+	pubEach := flag.Int("pubeach", 1, "How many publish one client do")
+	interval := flag.Int("interval", 1000, "Interval of publishes(when [pubeach] > 1), millisecond")
 
-	reg := flag.Bool("reg", false, "If register and save the returned infomation")
+	reg := flag.Bool("reg", false, "Only register and save the infomation")
 	file := flag.String("file", "./reg.info", "Register infomation file")
 	//retained := flag.Bool("retained", false, "Are the messages sent with the retained flag")
 	flag.Parse()
@@ -137,6 +160,17 @@ func main() {
 		wgReg.Wait()
 		// regFile.Sync()
 	} else {
+		if *msgLen < 8 {
+			*msgLen = 8
+		}
+		pubTimes = make([][]int64, *pubClient)
+		for i := range pubTimes {
+			pubTimes[i] = make([]int64, *pubEach)
+		}
+		usedTimes = make([][]int64, *pubClient)
+		for i := range usedTimes {
+			usedTimes[i] = make([]int64, *pubEach)
+		}
 
 		regFile, err := os.Open(*file)
 		if err != nil {
@@ -148,16 +182,15 @@ func main() {
 		wgRecv.Add(1)
 		cnt := 0
 		for fileScanner.Scan() {
-			
 			regInfo := strings.Split(fileScanner.Text(), "|")
 			wgSub.Add(1)
 			wgWork.Add(1)
-			go doWork(cnt, &regInfo[0], &regInfo[1], &regInfo[2], broker, topic, *qos, message, *pubCnt, *interval)
+			go doWork(cnt, &regInfo[0], &regInfo[1], &regInfo[2], broker, topic, *qos, *msgLen, *pubEach, *interval, cnt < *pubClient)
 			time.Sleep(10 * time.Millisecond)
 			cnt++
 		}
 
-		msgNeedRecv := (cnt * *pubCnt * cnt)
+		msgNeedRecv := (*pubClient * *pubEach * cnt)
 
 		for {
 			time.Sleep(2 * time.Second)
@@ -168,6 +201,29 @@ func main() {
 		}
 		wgRecv.Done()
 		wgWork.Wait()
-		log.Printf("msgRecv: %d\n", msgRecv)
+		// dur := lastRecv.Sub(beginPub)
+		// mill := int64(dur / 1000000)
+
+		pubTotal := *pubEach * *pubClient
+
+		minTime := int64(math.MaxInt64)
+		maxTime := int64(math.MinInt64)
+		totalTime := int64(0)
+
+		for _, usedTime := range usedTimes {
+			for _, ms := range usedTime {
+				log.Printf("ms: %d\n", ms)
+				if minTime > ms {
+					minTime = ms
+				}
+				if maxTime < ms {
+					maxTime = ms
+				}
+				totalTime += ms
+			}
+		}
+		log.Printf("\n")
+		log.Printf("Published: %d, Received: %d", pubTotal, msgRecv)
+		log.Printf("Total: %d ms, Max: %d ms, Min: %d, Avg: %d ms\n", totalTime, maxTime, minTime, totalTime / int64(pubTotal))
 	}
 }
